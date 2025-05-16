c0c4386bed53f8c13fc4759ad43bb735f8ac548b
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-05-17 00:43:48 +0300

    fixes

--- presentation-features/presentation-api/src/commonMain/kotlin/com/grippo/presentation.api/auth/models/PasswordFormatState.kt
+++ presentation-features/presentation-api/src/commonMain/kotlin/com/grippo/presentation.api/auth/models/PasswordFormatState.kt
@@ -4,17 +4,17 @@ import androidx.compose.runtime.Immutable
 import com.grippo.validation.PasswordValidator
 
 @Immutable
-public sealed class PasswordState(public open val value: String) {
+public sealed class PasswordFormatState(public open val value: String) {
     public data class Valid(
         override val value: String
-    ) : PasswordState(value = value)
+    ) : PasswordFormatState(value = value)
 
     public data class Invalid(
         override val value: String
-    ) : PasswordState(value = value)
+    ) : PasswordFormatState(value = value)
 
     public companion object {
-        public fun of(value: String): PasswordState {
+        public fun of(value: String): PasswordFormatState {
             return if (PasswordValidator.isValid(value)) {
                 Valid(value)
             } else {
