b45b0307112f34358fbc09a5e51865f530154ba5
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-05-24 16:22:40 +0300

    fix

--- presentation-features/home/src/commonMain/kotlin/com/grippo/home/trainings/TrainingsScreen.kt
+++ presentation-features/home/src/commonMain/kotlin/com/grippo/home/trainings/TrainingsScreen.kt
@@ -1,5 +1,6 @@
 package com.grippo.home.trainings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.grippo.core.BaseComposeScreen
 import com.grippo.design.components.training.ExerciseCard
 import com.grippo.design.core.AppTokens
@@ -34,7 +36,8 @@ internal fun TrainingsScreen(
         contentPadding = PaddingValues(
             horizontal = AppTokens.dp.paddings.screenHorizontal,
             vertical = AppTokens.dp.paddings.screenVertical
-        )
+        ),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
 
         items(list, key = { it.id }) { exercise ->
