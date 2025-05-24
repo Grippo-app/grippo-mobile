c433057a76f67b95a6740ef6aec5e00ae38d0487
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-05-25 01:01:36 +0300

    remove odd

--- common/metrics/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
-}
-
-apply(from = "$rootDir/gradle/common/android.gradle")
-apply(from = "$rootDir/gradle/common/kotlin.gradle")
