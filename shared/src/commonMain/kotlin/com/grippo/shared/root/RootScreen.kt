2f377b5d017fa2db5b4d27398f53b6677bb9224a
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-06-09 00:53:46 +0300

    clear

--- shared/src/commonMain/kotlin/com/grippo/shared/root/RootScreen.kt
+++ shared/src/commonMain/kotlin/com/grippo/shared/root/RootScreen.kt
@@ -1,29 +1,23 @@
 package com.grippo.shared.root
 
 import androidx.compose.runtime.Composable
-import com.arkivanov.decompose.router.stack.ChildStack
-import com.arkivanov.decompose.value.Value
-import com.arkivanov.essenty.backhandler.BackHandler
 import com.grippo.core.BaseComposeScreen
 import com.grippo.design.core.AppTokens
 import com.grippo.platform.core.backAnimation
-import com.grippo.presentation.api.RootRouter
-import com.grippo.shared.root.RootComponent.Child
 import kotlinx.collections.immutable.ImmutableSet
 import com.arkivanov.decompose.extensions.compose.experimental.stack.ChildStack as ChildStackCompose
 
 @Composable
 internal fun RootScreen(
-    stack: Value<ChildStack<RootRouter, Child>>,
-    backHandler: BackHandler,
+    component: RootComponent,
     state: RootState,
-    contract: RootContract,
     loaders: ImmutableSet<RootLoader>,
+    contract: RootContract,
 ) = BaseComposeScreen(AppTokens.colors.background.primary) {
     ChildStackCompose(
-        stack = stack,
+        stack = component.childStack,
         animation = backAnimation(
-            backHandler = backHandler,
+            backHandler = component.backHandler,
             onBack = contract::back
         ),
         content = { child -> child.instance.component.Render() }
