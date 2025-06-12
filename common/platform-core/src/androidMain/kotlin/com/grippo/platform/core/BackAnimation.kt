f71453b2da50d32a7a0fb4f76f781f4857c0877d
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-06-12 23:04:47 +0300

    fix

--- common/platform-core/src/androidMain/kotlin/com/grippo/platform/core/BackAnimation.kt
+++ common/platform-core/src/androidMain/kotlin/com/grippo/platform/core/BackAnimation.kt
@@ -2,7 +2,9 @@ package com.grippo.platform.core
 
 import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.StackAnimation
 import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.plus
+import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.experimental.stack.animation.stackAnimation
 
 public actual fun <C : Any, T : Any> platformAnimation(): StackAnimation<C, T> =
-    stackAnimation(animator = fade())
+    stackAnimation(animator = fade() + slide())
