b99ccf490ac6d2ca3e371a78bd6dc05fe0ee4a56
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-07-20 13:35:26 +0300

    updates

--- design-system/components/src/commonMain/kotlin/com/grippo/design.components/bottomsheet/DragHandle.kt
+++ design-system/components/src/commonMain/kotlin/com/grippo/design.components/bottomsheet/DragHandle.kt
@@ -2,20 +2,18 @@ package com.grippo.design.components.bottomsheet
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.grippo.design.components.modifiers.scalableClick
+import com.grippo.design.components.button.Button
+import com.grippo.design.components.button.ButtonStyle
 import com.grippo.design.core.AppTokens
 import com.grippo.design.preview.AppPreview
 import com.grippo.design.preview.PreviewContainer
-import com.grippo.design.resources.icons.NavArrowLeft
 
 @Composable
 public fun DragHandle(
@@ -27,15 +25,13 @@ public fun DragHandle(
             .height(intrinsicSize = IntrinsicSize.Min),
     ) {
         onBack?.let {
-            Icon(
+            Button(
                 modifier = Modifier
                     .align(Alignment.CenterStart)
-                    .scalableClick(onClick = onBack)
-                    .fillMaxHeight()
                     .padding(horizontal = AppTokens.dp.screen.horizontalPadding),
-                imageVector = AppTokens.icons.NavArrowLeft,
-                contentDescription = null,
-                tint = AppTokens.colors.icon.primary,
+                onClick = onBack,
+                text = "Back",
+                style = ButtonStyle.Transparent
             )
         }
 
