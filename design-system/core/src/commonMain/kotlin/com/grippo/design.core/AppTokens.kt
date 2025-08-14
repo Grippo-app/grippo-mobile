5c76b9184dee312aee1e6f018ed3a1ead5d9b0e9
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-08-14 19:00:16 +0300

    design(core): mark AppTokens as @Stable and getters as @ReadOnlyComposable

--- design-system/core/src/commonMain/kotlin/com/grippo/design.core/AppTokens.kt
+++ design-system/core/src/commonMain/kotlin/com/grippo/design.core/AppTokens.kt
@@ -1,6 +1,8 @@
 package com.grippo.design.core
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.Stable
 import com.grippo.design.core.internal.LocalAppColors
 import com.grippo.design.core.internal.LocalAppDp
 import com.grippo.design.core.internal.LocalAppDrawables
@@ -14,28 +16,35 @@ import com.grippo.design.resources.provider.AppIcon
 import com.grippo.design.resources.provider.AppString
 import com.grippo.design.resources.provider.AppTypography
 
+@Stable
 public object AppTokens {
     public val colors: AppColor
         @Composable
+        @ReadOnlyComposable
         get() = LocalAppColors.current
 
     public val icons: AppIcon
         @Composable
+        @ReadOnlyComposable
         get() = LocalAppIcons.current
 
     public val typography: AppTypography
         @Composable
+        @ReadOnlyComposable
         get() = LocalAppTypography.current
 
     public val strings: AppString
         @Composable
+        @ReadOnlyComposable
         get() = LocalAppStrings.current
 
     public val drawables: AppDrawable
         @Composable
+        @ReadOnlyComposable
         get() = LocalAppDrawables.current
 
     public val dp: AppDp
         @Composable
+        @ReadOnlyComposable
         get() = LocalAppDp.current
 }
