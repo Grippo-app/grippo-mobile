cf9b10417660d260925e90a53d8821e6e248d022
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-08-27 12:35:20 +0300

    add mapper

---
+++ data-mappers/domain-to-state/src/commonMain/kotlin/com/grippo/domain/state/equipment/EquipmentGroupEnumMapper.kt
@@ -0,0 +1,14 @@
+package com.grippo.domain.state.equipment
+
+import com.grippo.data.features.api.equipment.models.EquipmentGroupEnum
+import com.grippo.state.equipments.EquipmentGroupEnumState
+
+public fun EquipmentGroupEnum.toState(): EquipmentGroupEnumState {
+    return when (this) {
+        EquipmentGroupEnum.FREE_WEIGHT -> EquipmentGroupEnumState.FREE_WEIGHT
+        EquipmentGroupEnum.MACHINES -> EquipmentGroupEnumState.MACHINES
+        EquipmentGroupEnum.CABLE_MACHINES -> EquipmentGroupEnumState.CABLE_MACHINES
+        EquipmentGroupEnum.BODY_WEIGHT -> EquipmentGroupEnumState.BODY_WEIGHT
+        EquipmentGroupEnum.BENCHES_AND_RACKS -> EquipmentGroupEnumState.BENCHES_AND_RACKS
+    }
+}
