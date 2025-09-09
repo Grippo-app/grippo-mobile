2828af45b55393fe1f6801775acf660a3b4d4b10
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-09-09 21:16:03 +0300

    last used

--- ui-dialog-features/exercise-example-picker/src/commonMain/kotlin/com/grippo/exercise/example/picker/ExerciseExamplePickerViewModel.kt
+++ ui-dialog-features/exercise-example-picker/src/commonMain/kotlin/com/grippo/exercise/example/picker/ExerciseExamplePickerViewModel.kt
@@ -55,7 +55,7 @@ public class ExerciseExamplePickerViewModel(
             .safeLaunch()
 
         safeLaunch {
-            exerciseExampleFeature.getExerciseExamples().getOrThrow()
+            exerciseExampleFeature.getExerciseExamples()
         }
     }
 
