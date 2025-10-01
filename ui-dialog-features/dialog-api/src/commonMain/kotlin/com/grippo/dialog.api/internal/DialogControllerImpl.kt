f337e0fe40244dd24f5d1b0a9d9ec96d59437665
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-10-01 23:11:11 +0300

    rollback dialog

--- ui-dialog-features/dialog-api/src/commonMain/kotlin/com/grippo/dialog.api/internal/DialogControllerImpl.kt
+++ ui-dialog-features/dialog-api/src/commonMain/kotlin/com/grippo/dialog.api/internal/DialogControllerImpl.kt
@@ -7,15 +7,16 @@ import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import org.koin.core.annotation.Single
 
 @Single(binds = [DialogController::class, DialogProvider::class])
 internal class DialogControllerImpl : DialogController, DialogProvider {
 
     private val _dialog = Channel<DialogConfig>(Channel.CONFLATED)
-    override val dialog: Flow<DialogConfig> = _dialog
+    override val dialog: Flow<DialogConfig> = _dialog.receiveAsFlow()
 
     override fun show(config: DialogConfig) {
-        _dialog.tryEmit(config)
+        _dialog.trySend(config)
     }
 }
