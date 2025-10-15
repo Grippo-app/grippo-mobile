a48176cd4656d8a06b058485a4a73dc67c162eda
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-10-15 15:20:30 +0300

    fix image loader size

--- toolkit/image-loader/src/commonMain/kotlin/com/grippo/toolkit/image/loader/ImageLoaderModule.kt
+++ toolkit/image-loader/src/commonMain/kotlin/com/grippo/toolkit/image/loader/ImageLoaderModule.kt
@@ -3,6 +3,9 @@ package com.grippo.toolkit.image.loader
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import coil3.network.ktor3.KtorNetworkFetcherFactory
+import coil3.request.crossfade
+import coil3.request.maxBitmapSize
+import coil3.size.Size
 import com.grippo.toolkit.http.client.HttpModule
 import com.grippo.toolkit.image.loader.internal.ImageLoaderInitializer
 import io.ktor.client.HttpClient
@@ -20,13 +23,15 @@ public class ImageLoaderModule {
     ): SingletonImageLoader.Factory {
         return SingletonImageLoader.Factory { context ->
             ImageLoader.Builder(context)
+                .crossfade(true)
+                .maxBitmapSize(Size(400,400))
                 .components { add(KtorNetworkFetcherFactory(httpClient)) }
                 .build()
         }
     }
 
     @Single(createdAtStart = true)
-    internal fun imageLoaderInitializer(
-        factory: SingletonImageLoader.Factory,
-    ): ImageLoaderInitializer = ImageLoaderInitializer(factory)
+    internal fun imageLoaderInitializer(factory: SingletonImageLoader.Factory): ImageLoaderInitializer {
+        return ImageLoaderInitializer(factory)
+    }
 }
--- toolkit/image-loader/src/commonMain/kotlin/com/grippo/toolkit/image/loader/internal/ImageLoaderInitializer.kt
+++ toolkit/image-loader/src/commonMain/kotlin/com/grippo/toolkit/image/loader/internal/ImageLoaderInitializer.kt
@@ -2,9 +2,7 @@ package com.grippo.toolkit.image.loader.internal
 
 import coil3.SingletonImageLoader
 
-internal class ImageLoaderInitializer(
-    factory: SingletonImageLoader.Factory,
-) {
+internal class ImageLoaderInitializer(factory: SingletonImageLoader.Factory) {
     init {
         SingletonImageLoader.setSafe(factory)
     }
