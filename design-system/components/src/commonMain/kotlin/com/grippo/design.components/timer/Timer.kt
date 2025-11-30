5b412b69f495a7ebf7f51acc153e307d4023aafd
maxvoitenko <maxim.voitenko@planetvpn.dev>
2025-11-30 13:30:04 +0200

    fix timer

--- design-system/components/src/commonMain/kotlin/com/grippo/design.components/timer/Timer.kt
+++ design-system/components/src/commonMain/kotlin/com/grippo/design.components/timer/Timer.kt
@@ -1,14 +1,20 @@
 package com.grippo.design.components.timer
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import com.grippo.design.core.AppTokens
 import com.grippo.design.preview.AppPreview
 import com.grippo.design.preview.PreviewContainer
+import com.grippo.design.resources.provider.icons.Timer
 import com.grippo.toolkit.date.utils.DateTimeUtils
 import com.grippo.toolkit.date.utils.timerTextFlow
 import kotlinx.datetime.LocalDateTime
@@ -21,13 +27,26 @@ public fun Timer(
     val timerFlow = remember(value) { timerTextFlow(start = value) }
     val time = timerFlow.collectAsState(initial = "")
 
-    Text(
-        text = time.value,
-        style = AppTokens.typography.b14Semi(),
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        color = AppTokens.colors.text.secondary
-    )
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(AppTokens.dp.timeLabel.spacer)
+    ) {
+        Icon(
+            modifier = Modifier.size(AppTokens.dp.timeLabel.icon),
+            tint = AppTokens.colors.icon.secondary,
+            imageVector = AppTokens.icons.Timer,
+            contentDescription = null
+        )
+
+        Text(
+            text = time.value,
+            style = AppTokens.typography.b14Semi(),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = AppTokens.colors.text.secondary
+        )
+    }
 }
 
 @AppPreview
