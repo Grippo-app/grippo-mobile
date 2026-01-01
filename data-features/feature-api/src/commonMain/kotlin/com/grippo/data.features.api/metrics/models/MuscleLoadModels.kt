ca5a752f18e5f3b5aaaa05205f153bf70143a15a
maxvoitenko <maxim.voitenko@planetvpn.dev>
2026-01-01 22:14:51 +0200

    clear everything

--- data-features/feature-api/src/commonMain/kotlin/com/grippo/data.features.api/metrics/models/MuscleLoadModels.kt
+++ data-features/feature-api/src/commonMain/kotlin/com/grippo/data.features.api/metrics/models/MuscleLoadModels.kt
@@ -2,17 +2,17 @@ package com.grippo.data.features.api.metrics.models
 
 import com.grippo.data.features.api.muscle.models.MuscleEnum
 
-public data class MuscleLoadEntry(
-    val label: String,
-    val percentage: Float,
-    val muscles: List<MuscleEnum>,
+public data class MuscleLoadSummary(
+    val perGroup: MuscleLoadBreakdown,
+    val perMuscle: MuscleLoadBreakdown,
 )
 
 public data class MuscleLoadBreakdown(
     val entries: List<MuscleLoadEntry>,
 )
 
-public data class MuscleLoadSummary(
-    val perGroup: MuscleLoadBreakdown,
-    val perMuscle: MuscleLoadBreakdown,
+public data class MuscleLoadEntry(
+    val label: String,
+    val percentage: Float,
+    val muscles: List<MuscleEnum>,
 )
