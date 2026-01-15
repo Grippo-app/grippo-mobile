193806a4efc6106dd6a470487a7316e57563a3ac
maxvoitenko <maxim.voitenko@planetvpn.dev>
2026-01-15 22:40:45 +0200

    fix calculation

--- ui-screen-features/home/src/commonMain/kotlin/com/grippo/home/home/HomeState.kt
+++ ui-screen-features/home/src/commonMain/kotlin/com/grippo/home/home/HomeState.kt
@@ -12,7 +12,7 @@ import kotlin.time.Duration
 
 @Immutable
 internal data class HomeState(
-    val range: DateRange.Range = DateRange.Range.Last7Days(),
+    val range: DateRange.Range = DateRange.Range.Monthly(),
     val lastTraining: TrainingState? = null,
     val digest: DigestState? = null,
     val totalDuration: Duration? = null,
