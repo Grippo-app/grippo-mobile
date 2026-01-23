3ab77a919c996c7cb1bef0ae4b013069669b9ba8
maxvoitenko <maxim.voitenko@planetvpn.dev>
2026-01-23 14:22:46 +0200

    clear

--- design-system/components/src/commonMain/kotlin/com/grippo/design.components/chip/ForceTypeChip.kt
+++ design-system/components/src/commonMain/kotlin/com/grippo/design.components/chip/ForceTypeChip.kt
@@ -1,9 +1,16 @@
 package com.grippo.design.components.chip
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.dp
 import com.grippo.core.state.examples.ForceTypeEnumState
 import com.grippo.design.core.AppTokens
 import com.grippo.design.preview.AppPreview
@@ -29,12 +36,25 @@ public fun ForceTypeChip(
         modifier = modifier,
         label = ChipLabel.Empty,
         value = value.title().text(),
-        trailing = ChipTrailing.Empty,
+        trailing = ChipTrailing.Content(
+            lambda = {
+                Box(
+                    Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                        .padding(5.dp)
+                        .background(
+                            color = value.color(),
+                            shape = CircleShape
+                        )
+                )
+            }
+        ),
         size = size,
         stype = ChipStype.Default,
         textColor = AppTokens.colors.static.white,
-        iconColor = AppTokens.colors.static.white,
-        brush = SolidColor(value.color())
+        iconColor = value.color(),
+        brush = SolidColor(AppTokens.colors.background.card)
     )
 }
 
