7e952f2712c3e6c7aab33419ba333f8493975d55
maxvoitenko <maxim.voitenko@planetvpn.dev>
2026-04-02 23:47:15 +0300

    push tokens

--- data-services/firebase/src/commonMain/kotlin/com/grippo/services/firebase/FirebaseAnalyticsProvider.kt
+++ data-services/firebase/src/commonMain/kotlin/com/grippo/services/firebase/FirebaseAnalyticsProvider.kt
@@ -1,5 +1,6 @@
 package com.grippo.services.firebase
 
 public interface FirebaseAnalyticsProvider {
+
     public fun logEvent(name: String, params: Map<String, String>)
 }
--- data-services/firebase/src/commonMain/kotlin/com/grippo/services/firebase/FirebaseMessagingProvider.kt
+++ data-services/firebase/src/commonMain/kotlin/com/grippo/services/firebase/FirebaseMessagingProvider.kt
@@ -2,6 +2,5 @@ package com.grippo.services.firebase
 
 public interface FirebaseMessagingProvider {
 
-    /** Returns the current FCM registration token, or null if unavailable. */
     public suspend fun getToken(): String?
 }
