82202ebda5259a75cd4d54cd387b032874beeb8e
maxvoitenko <maxim.voitenko@planetvpn.dev>
2026-04-05 13:44:19 +0300

    upd Android version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 
     defaultConfig {
         applicationId = "com.grippo.android"
-        versionCode = 16
-        versionName = "1.4"
+        versionCode = 17
+        versionName = "1.6"
         multiDexEnabled = true
         manifestPlaceholders["GOOGLE_SERVER_CLIENT_ID"] = "YOUR_GOOGLE_SERVER_CLIENT_ID"
     }
